5c1d1329ad2b88966e29b379ce1052e427955dd5
Matheus Hora <matheus11hora@gmail.com>
2024-02-05 19:11:36 -0300

    Subindo as modificações do outro projeto

--- ArteAcucarClient/src/app/app.module.ts
+++ ArteAcucarClient/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './page/display/display.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, DisplayComponent, SignInComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ArteAcucarClient/src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss',
+})
+export class SignInComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {}
+}
