ecd16764f80121aee5e60517e4fba9fd143bf528
Anderson Junior <acfaj2010@gmail.com>
2024-02-06 00:06:40 -0300

    correção de erro de rota

--- ArteAcucarClient/src/app/app-routing.module.ts
+++ ArteAcucarClient/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './page/sign-in/sign-in.component';
 import { DisplayComponent } from './page/display/display.component';
 import { PricingComponent } from './page/pricing/pricing.component';
-import { PricingComponent } from './page/pricing/pricing.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
