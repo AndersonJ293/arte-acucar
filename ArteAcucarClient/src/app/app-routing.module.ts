f1f987b292099bab014d1b7210c448aeb0a2f737
Matheus Hora <matheus11hora@gmail.com>
2024-02-06 00:18:44 -0300

    iniciando a criação da tela de orçamento

--- ArteAcucarClient/src/app/app-routing.module.ts
+++ ArteAcucarClient/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './page/sign-in/sign-in.component';
 import { DisplayComponent } from './page/display/display.component';
 import { PricingComponent } from './page/pricing/pricing.component';
+import { BudgetComponent } from './page/budget/budget.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
@@ -10,7 +11,10 @@ const routes: Routes = [
   {
     path: 'painel',
     component: DisplayComponent,
-    children: [{ path: 'precificacao', component: PricingComponent }],
+    children: [
+      { path: 'precificacao', component: PricingComponent },
+      { path: 'orcamento', component: BudgetComponent },
+    ],
   },
 ];
 
