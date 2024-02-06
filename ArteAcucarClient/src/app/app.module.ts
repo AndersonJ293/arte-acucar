ee07ad4e88c40f7315523eb3a617636e185027c4
Matheus Hora <matheus11hora@gmail.com>
2024-02-05 23:10:27 -0300

    criando a tela de precificação

--- ArteAcucarClient/src/app/app.module.ts
+++ ArteAcucarClient/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { MenuComponent } from './components/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { PricingComponent } from './page/pricing/pricing.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, DisplayComponent, SignInComponent],
+  declarations: [AppComponent, HeaderComponent, DisplayComponent, SignInComponent, PricingComponent],
   imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
