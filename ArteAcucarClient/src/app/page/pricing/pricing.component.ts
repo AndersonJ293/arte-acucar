ad14464da21a566135fa918e2f55dd927d1e83c0
Matheus Hora <matheus11hora@gmail.com>
2024-02-08 12:24:04 -0300

    feat: adicionando o floatButton em todas as telas

--- ArteAcucarClient/src/app/page/pricing/pricing.component.ts
+++ ArteAcucarClient/src/app/page/pricing/pricing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrl: './pricing.component.scss'
+  styleUrl: './pricing.component.scss',
 })
 export class PricingComponent {
-
+  faAdd = faAdd;
 }
