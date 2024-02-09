2c79147c631bb4951ef6ea3950e965df0aa7081e
Matheus Hora <matheus11hora@gmail.com>
2024-02-09 01:12:16 -0300

    feat: começando a criação do modal de orçamento

--- ArteAcucarClient/src/app/page/budget/budget-edit/budget-edit.component.ts
+++ ArteAcucarClient/src/app/page/budget/budget-edit/budget-edit.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-budget-edit',
   templateUrl: './budget-edit.component.html',
-  styleUrl: './budget-edit.component.scss'
+  styleUrl: './budget-edit.component.scss',
 })
 export class BudgetEditComponent {
+  open: boolean = false;
+  selectedItems: any[] = [];
 
+  items = [
+    { id: 1, name: 'Insumo 1', brand: 'Marca 1', value: 10, quantity: 1 },
+    { id: 2, name: 'Insumo 2', brand: 'Marca 2', value: 15, quantity: 1 },
+    { id: 3, name: 'Insumo 3', brand: 'Marca 3', value: 20, quantity: 1 },
+  ];
+
+  openModal() {
+    this.open = true;
+  }
+
+  handleSave(selectedItems: any[]) {
+    this.selectedItems = selectedItems;
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.open = false;
+  }
 }
--- ArteAcucarClient/src/app/page/budget/budget-edit/budget-modal/budget-modal.component.ts
+++ ArteAcucarClient/src/app/page/budget/budget-edit/budget-modal/budget-modal.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-budget-modal',
   templateUrl: './budget-modal.component.html',
-  styleUrl: './budget-modal.component.scss'
+  styleUrl: './budget-modal.component.scss',
 })
 export class BudgetModalComponent {
+  @Input() items: any[] = [];
+  @Input() selectedItems: any[] = [];
+  @Output() save: EventEmitter<any[]> = new EventEmitter<any[]>();
 
+  searchInput: string = '';
+
+  toggleSelection(item: any) {
+    item.selected = !item.selected;
+
+    if (item.selected) {
+      this.selectedItems.push(item);
+      return;
+    }
+
+    const index = this.selectedItems.findIndex((sItem) => sItem.id === item.id);
+    if (index !== -1) this.selectedItems.splice(index, 1);
+  }
+
+  saveSelection() {
+    this.save.emit(this.selectedItems);
+  }
 }
