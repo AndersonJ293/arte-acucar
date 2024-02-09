29a21aaaece4afa162ba14bf4cacb177dc926908
Anderson Junior <acfaj2010@gmail.com>
2024-02-09 17:29:36 -0300

    feat: implementar o firebase na tela pricing

--- ArteAcucarClient/src/app/page/pricing/pricing.component.ts
+++ ArteAcucarClient/src/app/page/pricing/pricing.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faAdd } from '@fortawesome/free-solid-svg-icons';
 import { MenuService } from '../../services/menu.service';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss',
 })
-export class PricingComponent {
+export class PricingComponent implements OnInit {
   faAdd = faAdd;
+  pricings: any[] = [];
+
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.firebaseService
+      .getCollectionWithIds('pricings')
+
+      .subscribe((data) => {
+        this.pricings = data;
+      });
+  }
 
   get menuOpen() {
     return MenuService.menuOpen;
