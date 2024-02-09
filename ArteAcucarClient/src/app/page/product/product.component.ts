c3e92300fa292244e0e159ec6675d829efb40f40
Matheus Hora <matheus11hora@gmail.com>
2024-02-09 14:55:22 -0300

    feat: criando a view de produto

--- ArteAcucarClient/src/app/page/product/product.component.ts
+++ ArteAcucarClient/src/app/page/product/product.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  faAdd = faAdd;
 
+  get menuOpen() {
+    return MenuService.menuOpen;
+  }
 }
