8feb37fb05e21b710acdea52ef79327e3becb247
Anderson Junior <acfaj2010@gmail.com>
2024-02-25 17:10:26 -0300

    feat: adicionar preenchimento dos campos no budget

--- ArteAcucarClient/src/app/page/budget-report/budget-report.component.ts
+++ ArteAcucarClient/src/app/page/budget-report/budget-report.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseService } from '../../services/firebase.service';
+import { DisplayComponent } from '../display/display.component';
 
 @Component({
   selector: 'app-budget-report',
   templateUrl: './budget-report.component.html',
-  styleUrl: './budget-report.component.scss'
+  styleUrl: './budget-report.component.scss',
 })
-export class BudgetReportComponent {
+export class BudgetReportComponent implements OnInit {
+  budgetId: string = '';
+  budget: any = {};
+  carregado: boolean = false;
 
+  constructor(
+    private route: ActivatedRoute,
+    private firebaseService: FirebaseService
+  ) {}
+
+  // get config() {
+  //   return DisplayComponent.config;
+  // }
+
+  ngOnInit(): void {
+    this.budgetId = this.route.snapshot.paramMap.get('id')!;
+
+    this.firebaseService
+      .getDocumentById('budgets', this.budgetId)
+      .subscribe((data) => {
+        this.budget = data;
+        this.carregado = true;
+      });
+  }
 }
