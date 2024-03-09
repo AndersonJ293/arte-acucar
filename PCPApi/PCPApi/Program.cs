6b41c854d963aaa2286d9073f0b56531d6cd83a5
Matheus Wisch <wisch.pg@outlook.com>
2024-03-09 11:45:11 -0300

    feat: criando repository generico + configuraçao program.cs

--- PCPApi/PCPApi/Program.cs
+++ PCPApi/PCPApi/Program.cs
@@ -1,8 +1,11 @@
+using PCPApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
--- PCPApi/PCPApi/Repositories/IRepository.cs
+++ PCPApi/PCPApi/Repositories/IRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace PCPApi.Repositories;
+﻿using System.Linq.Expressions;
 
-public interface IRepository
+namespace PCPApi.Repositories;
+
+public interface IRepository<T>
 {
-    
+    IEnumerable<T> GetAll();
+    T? Get(Expression<Func<T, bool>> predicate);
+    T Create(T entity);
+    T Update(T entity);
+    T Delete(T entity);
 }
